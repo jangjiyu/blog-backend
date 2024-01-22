2d90d00e0c974a6d1c74affb381d7ccc69af452b
Jiyu Jang <apryll7995@gmail.com>
2024-01-22 17:19:25 +0900

    undefined to null interceptor

---
+++ src/common/interceptors/undefined-to-null.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class undefinedToNullInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    return next.handle().pipe(
+      map((observable) => {
+        return observable === undefined ? null : observable;
+      }),
+    );
+  }
+}
