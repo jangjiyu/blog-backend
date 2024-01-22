475f5cd4b96018a2b768c1ba91ca91b84df87580
Jiyu Jang <apryll7995@gmail.com>
2024-01-22 23:44:37 +0900

    swagger config 모듈 분리

---
+++ src/common/config/swagger.config.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const swaggerConfig = (app: INestApplication<any>) => {
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('나의 개발로그 API 문서')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,15 @@ import { ConfigService } from '@nestjs/config';
 import { EnvKeys } from './common/const/env-keys.const';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { swaggerConfig } from './common/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>(EnvKeys.ENV_PORT) || 3000;
 
-  const config = new DocumentBuilder()
-    .setTitle('Blog API')
-    .setDescription('나의 개발로그 API 문서')
-    .setVersion('1.0')
-    .addCookieAuth('connect.sid')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  swaggerConfig(app);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
