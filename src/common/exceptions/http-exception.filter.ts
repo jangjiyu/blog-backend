5910f42646a700c8f193e6bc402e8835465528de
Jiyu Jang <apryll7995@gmail.com>
2024-01-30 20:09:54 +0900

    http exception filter response message 수정

--- src/common/exceptions/http-exception.filter.ts
+++ src/common/exceptions/http-exception.filter.ts
@@ -9,19 +9,27 @@ import {
 @Catch(HttpException) // HttpException에 해당하는 거 모두 catch해옴
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
-    console.log(exception.getResponse());
-
     const ctx = host.switchToHttp();
     const request = ctx.getRequest();
     const response = ctx.getResponse();
     const status = exception.getStatus();
+    const err = exception.getResponse() as
+      | { message: any; statusCode: number }
+      | { error: string; statusCode: 400; message: string[] }
+      | { error: string; statusCode: 404; message: string[] };
+
+    console.log(err, '// type:', typeof err);
+
+    let message = null;
+    if (typeof err !== 'string' && err) message = err.message;
+    else message = err;
 
     // 로그 파일을 생성하거나
     // 에러 모니터링 시스템에 api 콜하기 등 가능
 
     response.status(status).json({
       statusCode: status,
-      message: exception.message,
+      message,
       timestamp: new Date().toLocaleString('kr'),
       path: request.url,
     });
