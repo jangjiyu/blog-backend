92e7615c183afb0a2a3b7a1e1db85f54b9eeb7b8
Jiyu Jang <apryll7995@gmail.com>
2024-02-20 19:41:15 +0900

    주석 수정

--- src/auth/guard/not-logged-in.guard.ts
+++ src/auth/guard/not-logged-in.guard.ts
@@ -7,6 +7,6 @@ export class NotLoggedInGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    return !request.isAuthenticated(); // LocalStrategy에서 false를 반환한 경우 로그인 안 된 상태이므로 가드 통과 못하게
+    return !request.isAuthenticated(); // LocalStrategy에서 false를 반환한 경우 로그인 안 된 상태이므로 가드 통과
   }
 }
