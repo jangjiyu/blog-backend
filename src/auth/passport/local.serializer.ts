02ee502b0e83f3541d995e3711fcb8d2cfee2b7d
Jiyu Jang <apryll7995@gmail.com>
2024-02-20 19:41:46 +0900

    사용 안 하는 코드 제거

--- src/auth/passport/local.serializer.ts
+++ src/auth/passport/local.serializer.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PassportSerializer } from '@nestjs/passport';
-import { AuthService } from '../auth.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/users.entity';
 import { Repository } from 'typeorm';
@@ -8,7 +7,6 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class LocalSerializer extends PassportSerializer {
   constructor(
-    private readonly authService: AuthService,
     @InjectRepository(UserEntity) private readonly User: Repository<UserEntity>,
   ) {
     super();
