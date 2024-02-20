6ab8b0ccca5ed51a6638c84f0bfd62a658cecfad
Jiyu Jang <apryll7995@gmail.com>
2024-02-20 19:42:32 +0900

    환경변수 validation 수정

--- src/config/env/validation-schema.ts
+++ src/config/env/validation-schema.ts
@@ -2,16 +2,16 @@ import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
   NODE_ENV: Joi.string().required(),
-  PORT: Joi.string().required(),
-  SALT: Joi.string().required(),
+  PORT: Joi.number().required(),
+  SALT: Joi.number().required(),
   SESSION_SECRET: Joi.string().required(),
   DB_PG_USERNAME: Joi.string().required(),
   DB_PG_PASSWORD: Joi.string().required(),
   DB_PG_HOST: Joi.string().required(),
-  DB_PG_PORT: Joi.string().required(),
+  DB_PG_PORT: Joi.number().required(),
   DB_PG_DATABASE: Joi.string().required(),
   DB_REDIS_HOST: Joi.string().required(),
-  DB_REDIS_PORT: Joi.string().required(),
+  DB_REDIS_PORT: Joi.number().required(),
   SWAGGER_API_USER: Joi.string().required(),
   SWAGGER_API_PASSWORD: Joi.string().required(),
 });
