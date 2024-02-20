7b0fd2460c313567ca9aba2f604d814970fc274e
Jiyu Jang <apryll7995@gmail.com>
2024-02-20 19:43:30 +0900

    ServeStaticModule로 프로필 파일 서빙

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -11,6 +11,9 @@ import databaseConfig from './config/env/database.config';
 import appConfig from './config/env/app.config';
 import authConfig from './config/env/auth.config';
 import { validationSchema } from './config/env/validation-schema';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -23,11 +26,23 @@ import { validationSchema } from './config/env/validation-schema';
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
+    ServeStaticModule.forRoot({
+      // 지정한 path를 클라이언트에서 접근 가능하게 함
+      rootPath: PUBLIC_FOLDER_PATH,
+      // rootPath로 /public 설정해도 http://localhost:3000/public/profile/~~.jpg가 아닌 http://localhost:3000/profile/~~.jpg로 됨 -> serveRoot로 '/public' 추가
+      serveRoot: '/public',
+    }),
     UsersModule,
     PostsModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
